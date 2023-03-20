9955e73a42121a29bbaafce79cf9da06c0a3f233
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2023-03-20 17:33:31 +0000

    Create SQLQuery4.sql

---
+++ AcademyDatabase/SQLQuery4.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE Trainees
+
+CREATE TABLE [dbo].[Trainees] (
+    [TraineeID] CHAR (5) NOT NULL,
+    [Name]      VARCHAR (20) NULL,
+    [Course]    VARCHAR (20) NULL,
+    [Location]  VARCHAR (20) NULL,
+    CONSTRAINT [PK_Trainees] PRIMARY KEY CLUSTERED ([TraineeID] ASC)
+);
